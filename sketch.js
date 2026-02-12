f106a4298b570b93204a90e43ccad31a8b8c3a82
Daniel Howe <dhowe@users.noreply.github.com>
2026-02-12 20:28:09 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,36 @@
-let len = 150;
+let walkers = [];
 
 function setup() {
-  createCanvas(700, 1300);
-  background(220);
-  noFill();
-  for (let i = 0; i < 12; i++) {
-    for (let j = 0; j < 24; j++) {
-      push();
-      translate(50+i*50,50+j*50);
-      square(0,0,50);
-      pop();
-    }
+  createCanvas(500, 500);
+  noStroke();
+  for (let i = 0; i < 10; i++) {
+    walkers[i] = {
+      x: width / 2,
+      y: height / 2,
+      tx: random(50, 450),
+      ty: random(50, 450),
+    };
   }
 }
 
+function draw() {
+  background(0, 32);
+
+  for (let i = 0; i < walkers.length; i++) {
+    let w = walkers[i];
+
+    circle(w.x, w.y, 10);
+
+    // move 5% of the way to the target
+    w.x = lerp(w.x, w.tx, 0.05);
+    w.y = lerp(w.y, w.ty, 0.05);
+
+    let d = dist(w.x, w.y, w.tx, w.ty);
+
+    // if we're close enough, pick a new target
+    if (d < 10) {
+      w.tx = random(width);
+      w.ty = random(height);
+    }
+  }
+}
